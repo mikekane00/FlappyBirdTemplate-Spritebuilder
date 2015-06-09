653ef6f738c9ac9c66c948bb05fc4fbd12c7172c
Mike Kane <mediocrecomedy@gmail.com>
2015-06-08 22:51:30 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -8,6 +8,7 @@
 {
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
+    [self addObstacle];
 }
 
 -(void)update:(CCTime)delta
@@ -16,5 +17,9 @@
 }
 
 // put new methods here
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
