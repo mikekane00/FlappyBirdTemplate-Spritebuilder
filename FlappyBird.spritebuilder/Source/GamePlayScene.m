16dde7362d5f4ec764679ae733e20ec214bf4775
Mike Kane <mediocrecomedy@gmail.com>
2015-06-09 10:34:31 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,12 +9,25 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    //Check to see if two seconds have elapsed
+    if (timeSinceObstacle > 2.0f)
+    {
+        //Add a new obstacle
+        [self addObstacle];
+        
+        //Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
 }
+
 
 // put new methods here
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
